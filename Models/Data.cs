20ae9a6d58f25cb7c41086afe7f91f0549b6d192
Olexiy Zavalniuk <83496121+OlexiyZavalniuk@users.noreply.github.com>
2023-02-19 17:44:13 +0200

    Add data class and config file

---
+++ Models/Data.cs
@@ -0,0 +1,24 @@
+﻿namespace Models
+{
+	public class Payer
+	{
+		public string Name { get; set; }
+		public decimal Payment { get; set; }
+		public DateTime Date { get; set; }
+		public long Account_number { get; set; }
+	}
+
+	public class Service
+	{
+		public string Name { get; set; }
+		public List<Payer> Payers { get; set; }
+		public decimal Total { get; set; }
+	}
+
+	public class Data
+	{
+		public string City { get; set; }
+		public List<Service> Services { get; set; }
+		public decimal Total { get; set; }
+	}
+}
